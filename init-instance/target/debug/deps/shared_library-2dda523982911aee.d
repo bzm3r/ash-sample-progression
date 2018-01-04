a4b9ebbdcf3ca3c465c1a31b6bad4f3b78f6dca9
Brian Merchant <bhmerchant@gmail.com>
2018-01-04 09:01:40 -0800

    removed init-instance cargo target folder

--- init-instance/target/debug/deps/shared_library-2dda523982911aee.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Brian Merchant\ownCloud\PROJECT_0\ash-tutorial\init-instance\target\debug\deps\libshared_library-2dda523982911aee.rlib: C:\Users\Brian\ Merchant\.cargo\registry\src\github.com-1ecc6299db9ec823\shared_library-0.1.8\src\lib.rs C:\Users\Brian\ Merchant\.cargo\registry\src\github.com-1ecc6299db9ec823\shared_library-0.1.8\src\dynamic_library.rs
-
-C:\Users\Brian Merchant\ownCloud\PROJECT_0\ash-tutorial\init-instance\target\debug\deps\shared_library-2dda523982911aee.d: C:\Users\Brian\ Merchant\.cargo\registry\src\github.com-1ecc6299db9ec823\shared_library-0.1.8\src\lib.rs C:\Users\Brian\ Merchant\.cargo\registry\src\github.com-1ecc6299db9ec823\shared_library-0.1.8\src\dynamic_library.rs
-
-C:\Users\Brian\ Merchant\.cargo\registry\src\github.com-1ecc6299db9ec823\shared_library-0.1.8\src\lib.rs:
-C:\Users\Brian\ Merchant\.cargo\registry\src\github.com-1ecc6299db9ec823\shared_library-0.1.8\src\dynamic_library.rs:
--- init-instance/target/debug/init-instance.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Brian\ Merchant\ownCloud\PROJECT_0\ash-tutorial\init-instance\target\debug\init-instance.exe: C:\Users\Brian\ Merchant\ownCloud\PROJECT_0\ash-tutorial\init-instance\src\main.rs
